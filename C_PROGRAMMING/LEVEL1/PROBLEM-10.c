08b28fb410dfba21fbf9d6dd2da56fd8fb2b5b7c
Harilp <harilp427@email.com>
2026-01-11 12:47:45 +0530

    problem solved

--- C_PROGRAMMING/LEVEL1/PROBLEM-10.c
+++ C_PROGRAMMING/LEVEL1/PROBLEM-10.c
@@ -1,7 +1,12 @@
-
+//get the 3 digit from the user,print the ten's digit
 #include <stdio.h>
 int main()
 {
-    
+    int a,b,c;
+    printf("enter the 3 digit number:");
+    scanf("%d",&a);
+    b=a%100;
+    c=b/10;
+    printf("the ten's digit number is:%d",c);
     return 0;
 }
