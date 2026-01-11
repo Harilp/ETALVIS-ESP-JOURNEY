c82f34cc84edda34c69c430f34b06d5b7fcc53cb
Harilp <harilp427@email.com>
2026-01-11 16:25:00 +0530

    problem 11 added

--- C_PROGRAMMING/LEVEL1/PROBLEM-11.c
+++ C_PROGRAMMING/LEVEL1/PROBLEM-11.c
@@ -2,10 +2,14 @@
 #include <stdio.h>
 int main()
 {
-    int a,b,c;
+    int a,b,c,e,f;
+    printf("enter the 3 digit number:\n");
     scanf("%d",&a);
     b=a%100;
     c=a/100;
-    printf("b:%d\nc:%d",b,c);
+    e=b/10;
+    f=b%10;
+    int sum = c+e+f;
+    printf("the sum of the digits are:%d\n",sum);
     return 0;
 }
