340922715ab9dc7d6c64a8989caa0734d3f8f378
Harilp <harilp427@email.com>
2026-01-11 16:40:15 +0530

    problem 12 added

--- C_PROGRAMMING/LEVEL1/PROBLEM-12.c
+++ C_PROGRAMMING/LEVEL1/PROBLEM-12.c
@@ -1,7 +1,26 @@
-
+//get the two digit number from the user and print the reverse of the number
 #include <stdio.h>
 int main()
 {
-    
+    int a,b,c,d,rev;
+    printf("enter the 2 digit number:\n");
+    scanf("%d",&a);
+    b=a/10;
+    c=a%10;
+    d=c*10;
+    rev=d+b;
+    printf("the reverse of the digits are:%d\n",rev);
+    return 0;
+}
+//or 
+#include <stdio.h>
+int main()
+{
+    int a,b,c,d,rev;
+    printf("enter the 2 digit number:\n");
+    scanf("%d",&a);
+    b=a/10;
+    c=a%10;
+    printf("the reverse of the digits are:%d%d\n",c,b);
     return 0;
 }
