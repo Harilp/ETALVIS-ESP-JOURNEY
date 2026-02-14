fa39c66facdd56b41e309a664c7275b264cf3360
Harilp <harilp427@gmail.com>
2026-02-14 19:22:40 +0530

    redo

--- C_PROGRAMMING/LEVEL1/PROBLEM-17.c
+++ C_PROGRAMMING/LEVEL1/PROBLEM-17.c
@@ -2,18 +2,14 @@
 #include <stdio.h>
     
 int main(){
-    int a,c;
+    int a;
     printf("enter the 3 digit number:");
     scanf("%d",&a);
     int u = (a/100)*100;
     int p = a%100;
     int d = (p/10)*10;
-    c=((p%10)*0)+2;
-    int r = c;
+    int c= 2;
     int result = u+d+c;
-    printf("result:%d",result);
-    
-    
-    
+    printf("result:%d",result);  
 return 0;
 }
