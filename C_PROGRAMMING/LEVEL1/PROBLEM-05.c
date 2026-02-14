e09b9821a715da10abaaa304fd1202d8ac9cc2de
HARI G <155338582+Harilp@users.noreply.github.com>
2026-02-14 16:29:20 +0530

    Add program to divide input by 8 and print result
    This program takes a user input number, divides it by 8, and prints the quotient.

---
+++ C_PROGRAMMING/LEVEL1/PROBLEM-05.c
@@ -0,0 +1,12 @@
+//get the number from the user,divide the number by 8,and print the remainder.
+//eg: input:45 output:5
+#include <stdio.h>
+  
+int main(){
+    int a,b;
+    printf("enter the number:");
+    scanf("%d",&a);
+    b=a/8;
+    printf("the remainder is:%d",b);
+
+}
