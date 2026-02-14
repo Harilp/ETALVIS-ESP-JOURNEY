6bd1ab0b20730f2530db79abffbf0124654def69
HARI G <155338582+Harilp@users.noreply.github.com>
2026-02-14 16:24:54 +0530

    Implement basic addition program in C

---
+++ C_PROGRAMMING/LEVEL1/PROBLEM-01.c
@@ -0,0 +1,11 @@
+//level 1-poblem 1 get a number from the user,add 2 to that number and print the result
+#include <stdio.h>
+#include <string.h>
+    
+int main(){
+    int a,b;
+    printf("enter a number:");
+    scanf("%d",&a);
+    b=a+2;
+    printf("the number is:%d",b);
+}
