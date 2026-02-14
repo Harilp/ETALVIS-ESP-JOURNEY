9a4925aa7b07e09f1b46c4db3e4fbe4e42f4beba
Harilp <harilp427@gmail.com>
2026-02-14 17:55:56 +0530

    redo

--- C_PROGRAMMING/LEVEL1/PROBLEM-14.c
+++ C_PROGRAMMING/LEVEL1/PROBLEM-14.c
@@ -1,15 +1,20 @@
-//get a four digit number from the user and print the reverse of it
+//get a four digit number from the user and reverse the first two digits of the number,print the result
 #include <stdio.h>
     
 int main(){
-    int a,b,c;
-    printf("enter the 3 digit number:");
+    int a,b,c,x,v,result;
+    printf("enter the 4 digit number:");
     scanf("%d",&a);
-    int x=a/100;
-    b=a%100;
-    int v=(b/10)*10;
-    c=(b%10)*100;
-    int result =c+v+x;
+    x=(a/1000);//n4
+    int q=x*1000;
+    b=a%1000;
+    v=(b/100);//n3
+    int w=v*100;
+    c=(b%100);
+    int r=c%10;//n2
+    int u=r*10;
+    int y=c/10;//n1
+    result = q+w+u+y;
     printf("result:%d",result);
 return 0;
 }
