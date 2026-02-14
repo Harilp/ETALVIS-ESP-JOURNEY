0ee4f2584a0d7c60882d2a1d3ffc4a9789049250
HARI G <155338582+Harilp@users.noreply.github.com>
2026-02-14 16:26:05 +0530

    Add program to subtract 5 from user input

---
+++ C_PROGRAMMING/LEVEL1/PROBLEM-02.c
@@ -0,0 +1,10 @@
+//get a number from the user ,subtract 5 from the number and print the result
+#include <stdio.h>  
+int main(){
+    printf("enter the number : ");
+    int a;
+    scanf("%d",&a);
+    int b=a-5;
+    printf("the number is : %d",b);
+    
+}
