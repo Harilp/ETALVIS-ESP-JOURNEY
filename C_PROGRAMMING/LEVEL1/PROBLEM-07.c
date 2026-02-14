60db4e746e1fdfec120bd7281af291b7c81a41a5
Harilp <harilp427@gmail.com>
2026-02-14 16:48:01 +0530

    problem-7

--- C_PROGRAMMING/LEVEL1/PROBLEM-07.c
+++ C_PROGRAMMING/LEVEL1/PROBLEM-07.c
@@ -1,1 +1,12 @@
-
+//get a two digit number from the user and print the ten's digit
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the2 digit number:");
+    scanf("%d",&a);
+    b=a/10;
+    printf("result:%d",b);
+    
+    return 0;
+}
