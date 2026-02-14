b3737b234af9c3f74a71c713569a5d064ba0d031
HARI G <155338582+Harilp@users.noreply.github.com>
2026-02-14 16:28:30 +0530

    Implement division of user input by 6

--- C_PROGRAMMING/LEVEL1/PROBLEM-04.c
+++ C_PROGRAMMING/LEVEL1/PROBLEM-04.c
@@ -1,1 +1,12 @@
+//get a number from the user , divide the number by 6 and print the result
+//eg: input:45 output:7
+#include <stdio.h>
 
+int main(){
+    printf("enter the number:");
+    int a;
+    scanf("%d",&a);
+    int b=a/6;
+    printf("the output is:%d",b);
+
+}
