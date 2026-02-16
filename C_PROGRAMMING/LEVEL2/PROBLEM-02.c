093bc539b3dc051584c3cd2daf50e7e21d4fb893
Harilp <harilp427@gmail.com>
2026-02-16 17:13:35 +0530

    redo

--- C_PROGRAMMING/LEVEL2/PROBLEM-02.c
+++ C_PROGRAMMING/LEVEL2/PROBLEM-02.c
@@ -1,11 +1,11 @@
-//get a number from the user and check if the number equals 50. if yes, the print 1,otherwise print 0
+//get a number from the user and check if the number not equals to 50. if yes, the print 1,otherwise print 0
 #include <stdio.h>
     
 int main(){
     printf("enter the number:");
     int a;
     scanf("%d",&a);
-    int b= (a == 50);
+    int b= (a != 50);
     printf("%d",b);
 return 0;
 }
