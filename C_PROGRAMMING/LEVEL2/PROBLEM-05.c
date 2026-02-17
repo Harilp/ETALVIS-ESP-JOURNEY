123c011a3fbb1467a3e44a039a4661d7e18772bc
Harilp <harilp427@gmail.com>
2026-02-17 16:52:43 +0530

    redo

---
+++ C_PROGRAMMING/LEVEL2/PROBLEM-05.c
@@ -0,0 +1,11 @@
+/*Get a number from the user and check if the number is less than or equal to 50. If yes, then print 1; otherwise, print 0.*/
+#include <stdio.h>
+int main()
+{
+    printf("enter the number:");
+    int a;
+    scanf("%d",&a);
+    int b= (a <= 50);
+    printf("result:%d",b);
+    return 0;
+}
