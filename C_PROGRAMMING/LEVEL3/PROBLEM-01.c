04238143e5942d4db61d8c4479e3cee3e84b71a2
HARI G <155338582+Harilp@users.noreply.github.com>
2026-02-20 00:35:40 +0530

    Implement number check for success or failure
    Added user input functionality to check if the number is 50.

--- C_PROGRAMMING/LEVEL3/PROBLEM-01.c
+++ C_PROGRAMMING/LEVEL3/PROBLEM-01.c
@@ -1,7 +1,15 @@
-//
+/*get a number from the user check if the number equals 50if yes print "success" other wise print "failure"*/
 #include <stdio.h>
-    
-int main(){
-    
-return 0;
+
+int main() {
+    int a;
+    printf("enter the number:");
+    scanf("%d",&a);
+    if(a == 50){
+        printf("success");
+    }
+    else{
+        printf("failure");
+    }
+    return 0;
 }
