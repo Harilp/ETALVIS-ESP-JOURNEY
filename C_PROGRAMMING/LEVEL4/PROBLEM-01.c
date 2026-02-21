15c25606b9c3db8a5dd897bb4c6f7866f408687a
Harilp <harilp427@gmail.com>
2026-02-21 11:44:48 +0530

    problem-01 solved

--- C_PROGRAMMING/LEVEL4/PROBLEM-01.c
+++ C_PROGRAMMING/LEVEL4/PROBLEM-01.c
@@ -1,17 +1,13 @@
+/*write a loop program to print 1 to 5 on one by one*/
 #include <stdio.h>
-
-int main() {
-
-    int x = 5;
-
-A:
-    printf("%d", x);
-    x--;
-
-    if (x > 0)
-        goto A;
-
-    printf("Done");
-
-    return 0;
+    
+int main(){
+    int x;
+    x = 1;
+    loop: if(x <= 5){
+        printf("%d\n",x);
+        x++;
+        goto loop;
+    }
+return 0;
 }
