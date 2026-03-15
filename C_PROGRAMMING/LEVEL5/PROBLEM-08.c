7f29dfc2a20614b249fa824a054143279170cdc9
Harilp <harilp427@gmail.com>
2026-03-15 20:02:33 +0530

    solved problems

--- C_PROGRAMMING/LEVEL5/PROBLEM-08.c
+++ C_PROGRAMMING/LEVEL5/PROBLEM-08.c
@@ -1,17 +1,17 @@
-/*write a loop program to print the two-digit odd numbers who's sum of the digit's are 7 */
+/*write a loop program to print the two-digit even numbers who's sum of the digit's are 6 */
 #include <stdio.h>
 int main()
 {
     int a,b,c,d,s;
-    for(a=10;a<=70;a++)
+    for(a=10;a<=60;a++)
     {
         b = a%2;
-        if(b==1)
+        if(b==0)
         {
             c=a%10;
             d=a/10;
             s = c+d;
-            if(s==7)
+            if(s==6)
             {
                 printf("%d\n",a);
             }
