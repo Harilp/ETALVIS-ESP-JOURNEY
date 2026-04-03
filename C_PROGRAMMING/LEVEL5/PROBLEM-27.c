120cd7c946f954d1ad5e92aa83841aba6cf41ae0
Harilp <harilp427@gmail.com>
2026-04-03 13:17:22 +0530

    solved problem

---
+++ C_PROGRAMMING/LEVEL5/PROBLEM-27.c
@@ -0,0 +1,27 @@
+/*Write a program to print the total count of numbers which are less than 100000 and whose sum of digits is 14.*/
+#include <stdio.h>
+int main()
+{
+    int a,b,c,sum,t,x;
+    x = 0;
+    for(c=1;c<100000;c++)
+    {
+        t=c;
+        sum  = 0;
+       for(;t!=0;)
+       {
+            b = t%10;
+            t = t/10;
+            sum = sum+b;
+       }
+       if(sum==14)
+       {
+         x = x+1;
+       }
+
+   }
+   
+   printf("%d",x);
+    
+    return 0;
+}
