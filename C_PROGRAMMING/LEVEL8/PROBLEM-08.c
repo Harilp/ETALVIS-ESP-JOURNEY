4b41cf8b61737baa7cff849c66ea14c659a4a2c2
Harilp <harilp427@gmail.com>
2026-05-31 17:37:55 +0530

    modified

--- C_PROGRAMMING/LEVEL8/PROBLEM-08.c
+++ C_PROGRAMMING/LEVEL8/PROBLEM-08.c
@@ -12,7 +12,7 @@ int main()
 }
 int disp_interchange_first_last_digit(int a)
 {
-    int b,c,d,e,f,g,h,sum;
+    int b,c,d,e,f,g,sum;
     c=1;
     b=a;
     while(a>9)
@@ -20,12 +20,10 @@ int disp_interchange_first_last_digit(int a)
         a=a/10;
         c=c*10;
     }
-    d=b/c;//1
-    e=b%c;//2345
-    g=e;
-    g=(g/10)*10;//234
-    f=e%10;//5
-    h=(e%g)*c;//50000
-    sum=h+g+d;
+    d=b/c;
+    e=b%c;
+    f=(e%10)*c;
+    g=(e/10)*10;
+    sum=f+g+d;
     return sum;
 }
