b0990d6439ed8b1f8dbac507a949430773e0ea92
Harilp <harilp427@gmail.com>
2026-06-10 16:22:32 +0530

    solved problem

---
+++ C_PROGRAMMING/LEVEL9/PROBLEM-03.c
@@ -0,0 +1,23 @@
+/*get 5 numbers from the user and print the smallest number*/
+#include <stdio.h>
+int main()
+{
+    int a[5],i,c,b;
+    printf("Enter the number:");
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    c=0;
+    b=a[0];
+    for(i=0;i<5;i++)
+    {
+        if(a[i]<=b)
+        {
+            b=a[i];
+        }    
+    }
+    printf("the smallest number is :%d",b);
+    
+    return 0;
+}
