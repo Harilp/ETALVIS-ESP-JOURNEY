4a6c903a8cf20be61e7d5610af9338b49cb29684
Harilp <harilp427@gmail.com>
2026-08-05 11:00:47 +0530

    solved problem

--- C_PROGRAMMING/LEVEL10/PROBLEM-02.c
+++ C_PROGRAMMING/LEVEL10/PROBLEM-02.c
@@ -1,9 +1,9 @@
-/*Get an alphabet and print its ASCII value.*/
+/*Get a single number and print its ASCII value.*/
 #include <stdio.h>
 int main()
 {
     char a;
-    printf("Enter the character:\n");
+    printf("Enter the number:\n");
     scanf("%c",&a);
     printf("%d\n",a);
     return 0;
