075f475683782d80646dcf7c422c040d199caee1
Harilp <harilp427@gmail.com>
2026-08-17 22:33:35 +0530

    solved problem

--- C_PROGRAMMING/LEVEL_11/PROBLEM-01.c
+++ C_PROGRAMMING/LEVEL_11/PROBLEM-01.c
@@ -1,7 +1,14 @@
-/**/
+/*Get a number from the user and print the same.
+Change the value without directly changing it and print the same.*/
 #include <stdio.h>
 int main()
 {
-    
+    int a,*b;
+    printf("Enter the number:");
+    scanf("%d",&a);
+    b=&a;
+    printf("Enter the number that have to be changed to:");
+    scanf("%d",&*b);
+    printf("%d",a);
     return 0;
 }
