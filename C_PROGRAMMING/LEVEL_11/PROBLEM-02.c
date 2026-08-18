86930c22ff8acb45442e3469c94868e528d475ab
Harilp <harilp427@gmail.com>
2026-08-19 01:27:11 +0530

    solved problem

--- C_PROGRAMMING/LEVEL_11/PROBLEM-02.c
+++ C_PROGRAMMING/LEVEL_11/PROBLEM-02.c
@@ -1,1 +1,24 @@
-/**/
+/*Pass an integer array to a function and increment each array element inside the function.*/
+#include <stdio.h>
+void pass(int a[]);
+int main()
+{
+    int a[5],i,x;
+    x=1;
+    for(i=0;i<5;i++)
+    {
+        a[i]=x;
+        x++;
+    }
+    pass(a);
+}
+void pass(int a[])
+{
+    int i;
+    for(i=0;i<5;i++)
+    {
+        a[i]++;
+        printf("%d\n",a[i]);
+    }
+
+}
